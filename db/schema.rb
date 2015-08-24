c07c51eb16d48362a7891f40e7b11d504e12a716
ocha <ochaochaocha3@gmail.com>
2015-08-25 00:59:36 +0900

    Firebird を DBMS として使う

--- db/schema.rb
+++ db/schema.rb
@@ -14,43 +14,43 @@
 ActiveRecord::Schema.define(version: 20150822145020) do
 
   create_table "families", force: :cascade do |t|
-    t.integer  "num_of_members",           default: 1,  null: false
-    t.integer  "at_home",        limit: 1, default: 0,  null: false
-    t.string   "address",                  default: "", null: false
-    t.string   "postal_code",    limit: 7, default: "", null: false
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
+    t.integer  "num_of_members", limit: 4,   default: 1,  null: false
+    t.integer  "at_home",        limit: 2,   default: 0,  null: false
+    t.string   "address",        limit: 255, default: "", null: false
+    t.string   "postal_code",    limit: 7,   default: "", null: false
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
   end
 
-  add_index "families", ["at_home"], name: "index_families_on_at_home"
-  add_index "families", ["num_of_members"], name: "index_families_on_num_of_members"
+  add_index "families", ["at_home"], name: "families_at_home"
+  add_index "families", ["num_of_members"], name: "families_num_of_members"
 
   create_table "leaders", force: :cascade do |t|
-    t.integer  "family_id",  null: false
-    t.integer  "refugee_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "family_id",  limit: 4, null: false
+    t.integer  "refugee_id", limit: 4, null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
-  add_index "leaders", ["family_id"], name: "index_leaders_on_family_id", unique: true
-  add_index "leaders", ["refugee_id"], name: "index_leaders_on_refugee_id", unique: true
+  add_index "leaders", ["family_id"], name: "leaders_family_id", unique: true
+  add_index "leaders", ["refugee_id"], name: "leaders_refugee_id", unique: true
 
   create_table "refugees", force: :cascade do |t|
     t.boolean  "presence",              default: true, null: false
     t.string   "name",       limit: 64, default: "",   null: false
     t.string   "furigana",   limit: 64, default: "",   null: false
-    t.integer  "gender",     limit: 1,  default: 0,    null: false
-    t.integer  "age"
+    t.integer  "gender",     limit: 2,  default: 0,    null: false
+    t.integer  "age",        limit: 4
     t.datetime "created_at",                           null: false
     t.datetime "updated_at",                           null: false
-    t.integer  "family_id",                            null: false
+    t.integer  "family_id",  limit: 4,                 null: false
   end
 
-  add_index "refugees", ["age"], name: "index_refugees_on_age"
-  add_index "refugees", ["family_id"], name: "index_refugees_on_family_id"
-  add_index "refugees", ["furigana"], name: "index_refugees_on_furigana"
-  add_index "refugees", ["gender"], name: "index_refugees_on_gender"
-  add_index "refugees", ["name"], name: "index_refugees_on_name"
-  add_index "refugees", ["presence"], name: "index_refugees_on_presence"
+  add_index "refugees", ["age"], name: "refugees_age"
+  add_index "refugees", ["family_id"], name: "refugees_family_id"
+  add_index "refugees", ["furigana"], name: "refugees_furigana"
+  add_index "refugees", ["gender"], name: "refugees_gender"
+  add_index "refugees", ["name"], name: "refugees_name"
+  add_index "refugees", ["presence"], name: "refugees_presence"
 
 end
