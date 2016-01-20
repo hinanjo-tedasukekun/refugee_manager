67dad33edfa62530b4442f61bb38e9c9f532d6ce
ocha <ochaochaocha3@gmail.com>
2016-01-20 21:34:09 +0900

    管理用ドメイン関連のファイルを消去する

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-
-  get 'refugees/query'
-  get 'refugees/input-num'
-
-  constraints Subdomain::Admin do
-    get 'refugees/count'
-    resources :refugees
-  end
-
   constraints subdomain: '' do
+    root 'welcome#index'
+
     get 'login' => 'refugee_sessions#new'
     post 'login' => 'refugee_sessions#create'
     delete 'logout' => 'refugee_sessions#destroy'
--- test/controllers/refugees_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class RefugeesControllerTest < ActionController::TestCase
-  test "should get count" do
-    get :count
-    assert_response :success
-  end
-
-end
