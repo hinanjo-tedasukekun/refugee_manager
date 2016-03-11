900985ab64f9ef551dbff80243c1a23ebba815bd
Kosuke Yamashita <ochaochaocha3@gmail.com>
2016-03-11 12:16:55 +0900

    CSV ファイルの MIME タイプを指定する

--- for_admin/app/controllers/for_admin/list_of_refugees_controller.rb
+++ for_admin/app/controllers/for_admin/list_of_refugees_controller.rb
@@ -8,7 +8,8 @@ module ForAdmin
     def all_refugees
       respond_to do |format|
         format.csv do
-          send_data(ListOfRefugees.all.encode('Shift_JIS'))
+          send_data(ListOfRefugees.all.encode('Shift_JIS'),
+                    type: 'text/csv')
         end
       end
     end
@@ -16,7 +17,8 @@ module ForAdmin
     def refugees_not_at_home
       respond_to do |format|
         format.csv do
-          send_data(ListOfRefugees.not_at_home.encode('Shift_JIS'))
+          send_data(ListOfRefugees.not_at_home.encode('Shift_JIS'),
+                    type: 'text/csv')
         end
       end
     end
@@ -24,7 +26,8 @@ module ForAdmin
     def refugees_at_home
       respond_to do |format|
         format.csv do
-          send_data(ListOfRefugees.at_home.encode('Shift_JIS'))
+          send_data(ListOfRefugees.at_home.encode('Shift_JIS'),
+                    type: 'text/csv')
         end
       end
     end
